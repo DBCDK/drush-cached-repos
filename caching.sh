df816369a8ea4dbe919d9c26df3eddb49e09936e
Michael Carlsson <mca@dbc.dk>
2012-03-12 14:27:23 +0100

    rewritten code - more functions

--- caching.sh
+++ caching.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# escaping special characters for grep
+escape() {
+  echo "$1" | sed 's/\([\.\$\*]\)/\\\1/g'
+}
+
+# is first argument in rest of arguments
+not_up_to_date() {
+  local item=$1
+  shift
+  ! echo " $@ " | grep -q " $(escape $item) "
+}
+
 # Find urls in make file ending which end with .git
 find_git_repos() {
-  RETURN=$( cat $1 | sed -e "/\.git[\"']* *$/ ! d; s/.*= *[\"']*\([^'\"]*\)[\"']* *$/\1/" )
+  RETURN=$(cat $1 | sed -e "/\.git[\"']* *$/ ! d; s/.*= *[\"']*\([^'\"]*\)[\"']* *$/\1/")
 }
 
 # Download git repository or fetch changi
@@ -29,23 +41,38 @@ translate_git_to_path() {
   RETURN=$( echo $git | sed "s/:\/\//-/; y/@:/-\//" )
 }
 
+repo_name() {
+  RETURN=$(echo "$1" | sed "s/.*\/\([^\/]*\)\.git$/\1/")
+}
+
+parse_makefile() {
+  makefile=$1
+  exclude=$2
+  local repos_downloaded
+  find_git_repos $makefile
+  local repos=$RETURN
+
+  for repo in $repos
+  do
+    if not_up_to_date $repo $exclude ; then
+      translate_git_to_path $repo
+      local path=$RETURN
+      download_git $repo $path
+      repo_name $repo
+      repos_downloaded="$repos_downloaded $RETURN"
+    fi
+  done
+
+  RETURN=$repos_downloaded
+}
+
 main() {
   local makefiles=$@
 
   for makefile in $makefiles
   do
-    find_git_repos $makefile
-    local repos=$RETURN
-
-    for repo in $repos
-    do
-      translate_git_to_path $repo
-      local path=$RETURN
-      download_git $repo $path
-    done
-
+    parse_makefile $makefile
   done
-
 }
 
 main "$@"
